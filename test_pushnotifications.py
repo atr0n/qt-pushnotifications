3a5a977293c2a72fe9e69a8d1f47cc7b65d5be5f
atr0n <albertotaberner@gmail.com>
2017-05-25 18:24:15 +0200

    Update test_pushnotifications.py
    Update for python 3 usage.

--- test_pushnotifications.py
+++ test_pushnotifications.py
@@ -1,5 +1,5 @@
-from apns2.client import APNsClient
-from apns2.payload import Payload
+from apns2 import APNSClient
+from apns2 import Payload
 from gcm import GCM
 import os
 
@@ -31,10 +31,10 @@ if(TARGET == "ios"):
     #creating a connection to the Apple Push notification service is really expensive and furthermore
     #has the disadvantage, that Apple detects multiple connection/disconnection attempts as possible DDos attack!
     #so we use a static variable to keep the connection open as long as possible.
-    @static_vars(client=APNsClient('apns.pem', use_sandbox=APNS_USE_SANDBOX, use_alternative_port=False))
+    @static_vars(client=APNSClient('apns.pem', use_sandbox=APNS_USE_SANDBOX, use_alternative_port=False))
     def sendAPNSNotification(token, message):
         payload = Payload(alert=message, sound="default", badge=0)
-        #client = APNsClient(APNS_CERT_PATH, use_sandbox=True, use_alternative_port=False)
+        #client = APNSClient(APNS_CERT_PATH, use_sandbox=True, use_alternative_port=False)
         sendAPNSNotification.client.send_notification(token, payload, topic=BUNDLE_ID)
 
 elif(TARGET == "android"): 
@@ -51,12 +51,12 @@ if __name__ == "__main__":
     token = "YOUR_TOKEN" #change that to your token
     message = "Hello World"
 
-    print "Sending '%s' to the phone with the token %s" %(message, token,)
+    print ("Sending '%s' to the phone with the token %s" %(message, token,))
 
     if(TARGET == "ios"):
 	   sendAPNSNotification(token, message)
     elif(TARGET == "android"):
         sendGCMNotification([token], message)
     else:
-        print "UNKNOWN TARGET"
+        print ("UNKNOWN TARGET")
 
